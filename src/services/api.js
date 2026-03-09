8c2dfd77790c1bdccd95032cc744335078512df4
shivasai0913 <shivasaigaine@gmail.com>
2026-03-10 00:56:32 +0530

    update

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://spotify-backend-main.onrender.com/api/songs";
+const BASE_URL = "https://spotify-backend-main.onrender.com/api/";
 //const BASE_URL = "http://localhost:8081/api";
 
 
